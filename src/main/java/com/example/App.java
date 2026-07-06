686dfd19d17f3a0e683d39bf2537cda2cacf8555
kickerbar <kicker5782@gmail.com>
2026-07-06 16:16:52 +0900

    Add files via upload

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", (exchange) -> {
+            String response = "<h1>NCP SourceDeploy Pure Java Test!</h1>";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+        System.out.println("Server started on port 8080");
+        server.start();
+    }
+}
